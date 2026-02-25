6f13b35d526ab093333292836ced8bd16253956c
Huseyin Yatar <huseyin.yatar@ogr.sakarya.edu.tr>
2026-02-25 17:17:02 +0300

    1295. Find Numbers with Even Number of Digits v2

--- src/main/java/org/findNumbers.java
+++ src/main/java/org/findNumbers.java
@@ -3,5 +3,15 @@ package org;
 public class findNumbers {
     public static void main(String[] args) {
 
+        int[] temp={555,901,482,1771};
+    }
+    static public int solution(int[] nums) {
+        int count=0;
+        for(Integer n: nums)
+        {
+            if((9<n&&n<100)||(999<n&&n<10000)||n==100000)
+                count++;
+        }
+        return count;
     }
 }
