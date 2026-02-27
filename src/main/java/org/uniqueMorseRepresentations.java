7db3c41f6b0da0894b359abd0fead0deb0404426
Huseyin Yatar <huseyin.yatar@ogr.sakarya.edu.tr>
2026-02-27 14:23:46 +0300

    804. Unique Morse Code Words

---
+++ src/main/java/org/uniqueMorseRepresentations.java
@@ -0,0 +1,27 @@
+package org;
+
+import java.util.HashSet;
+
+public class uniqueMorseRepresentations {
+
+    public static void main(String[] args) {
+        System.out.println(solution(new String[]{"qq","qq","qq","qq"}));
+    }
+
+    static public int solution(String[] words) {
+
+        String[] letters={".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."};
+        HashSet<String> set=new HashSet<>();
+        for(String s:words)
+        {
+            StringBuilder sd=new StringBuilder();
+            for(int i=0;i<s.length();i++)
+            {
+                int val=s.charAt(i)-'a';
+                sd.append(letters[val]);
+            }
+            set.add(sd.toString());
+        }
+        return set.size();
+    }
+}
