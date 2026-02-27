268064293ba38d14160aa60550c30376db66a3fa
Huseyin Yatar <huseyin.yatar@ogr.sakarya.edu.tr>
2026-02-27 14:34:11 +0300

    657. Robot Return to Origin

---
+++ src/main/java/org/judgeCircle.java
@@ -0,0 +1,24 @@
+package org;
+
+public class judgeCircle {
+    public static void main(String[] args) {
+
+        solution("UD");
+    }
+    static  public boolean solution(String moves) {
+        int horizontal = 0;
+        int vertical = 0;
+
+        for (char c : moves.toCharArray()) {
+            if (c == 'D')
+                vertical--;
+            else if (c == 'U')
+                vertical++;
+            else if (c == 'L')
+                horizontal--;
+            else
+                horizontal++;
+        }
+        return (Math.abs(vertical) + Math.abs(horizontal)) == 0;
+    }
+}
