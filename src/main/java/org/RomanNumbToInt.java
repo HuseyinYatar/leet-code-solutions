f807ff81ebf6961bc1a660dc34920f2b147faabd
Huseyin Yatar <huseyin.yatar@ogr.sakarya.edu.tr>
2026-02-27 14:32:38 +0300

    13. Roman to Integer

---
+++ src/main/java/org/RomanNumbToInt.java
@@ -0,0 +1,49 @@
+package org;
+
+
+import java.util.Map;
+
+public class RomanNumbToInt {
+    public static void main(String[] args) {
+
+        int x = romanToInt("MCDLXXVI");
+    }
+
+    static public int romanToInt(String s) {
+        Map<String, Integer> romanMap = Map.of(
+                "M", 1000,
+                "D", 500,
+                "C", 100,
+                "L", 50,
+                "X", 10,
+                "V", 5,
+                "I", 1
+        );
+
+        Map<String, Integer> specialRomanMap = Map.of(
+                "CM", 900,
+                "CD", 400,
+                "XC", 90,
+                "XL", 40,
+                "IX", 9,
+                "IV", 4
+        );
+        int sum = 0;
+        for (int i = 0; i < s.length() - 1; i++) {
+
+            String str = s.substring(i, i + 2);
+            if (specialRomanMap.containsKey(str)) {
+                sum += specialRomanMap.get(str);
+                i++;
+            } else {
+                sum += romanMap.get(str.substring(0, 1));
+            }
+        }
+        if (s.length() > 1 && !specialRomanMap.containsKey(s.substring(s.length() - 2))) {
+            sum += romanMap.get(s.substring(s.length() - 1));
+        }
+        if (s.length() == 1)
+            sum += romanMap.get(s);
+        return sum;
+    }
+}
