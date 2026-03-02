538db7c6bb6aefe78b6de1d486ce89e0533f1bb3
Huseyin Yatar <huseyin.yatar@ogr.sakarya.edu.tr>
2026-03-02 17:15:25 +0300

    905. Sort Array By Parity

---
+++ src/main/java/org/sortArrayByParity.java
@@ -0,0 +1,21 @@
+package org;
+
+public class sortArrayByParity {
+    public static void main(String[] args) {
+        solution(new int[]{1, 0});
+    }
+
+    static public int[] solution(int[] nums) {
+        for (int i = 0, j = nums.length - 1; i < j; i++) {
+            if (nums[i] % 2 == 1) {
+                while (nums[j] % 2 != 0 && i < j) {
+                    j--;
+                }
+                int temp = nums[j];
+                nums[j] = nums[i];
+                nums[i] = temp;
+            }
+        }
+        return nums;
+    }
+}
