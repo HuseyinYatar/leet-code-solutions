ea27734df5084743d97068cfb0832040086bc930
Huseyin Yatar <huseyin.yatar@ogr.sakarya.edu.tr>
2026-03-02 16:51:08 +0300

    344. Reverse String V2 more efficient version

--- src/main/java/org/reverseString.java
+++ src/main/java/org/reverseString.java
@@ -1,20 +1,15 @@
 package org;
 
-import java.util.Stack;
-
 public class reverseString {
     public static void main(String[] args) {
-        solution(new char[]{'h', 'e', 'l', 'l', 'o'});
+        solution(new char[]{'h', 'e','l', 'l', 'o'});
     }
 
     static public void solution(char[] s) {
-        Stack<Character> st = new Stack<>();
-        for (Character c : s) {
-            st.push(c);
+        for (int i = 0, j = s.length - 1; i < j; i++, j--) {
+            char temp = s[i];
+            s[i] = s[j];
+            s[j] = temp;
         }
-        for (int i = 0; i < s.length; i++) {
-            s[i] = st.pop();
-        }
-
     }
 }
