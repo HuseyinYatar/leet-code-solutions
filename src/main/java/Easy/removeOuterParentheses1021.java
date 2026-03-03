562296ef2160bd383dac5f0e5bfd72572c6558e1
Huseyin Yatar <huseyin.yatar@ogr.sakarya.edu.tr>
2026-03-03 11:31:07 +0300

    Time Complexity,O(n) Space Complexity,O(n)

---
+++ src/main/java/Easy/removeOuterParentheses1021.java
@@ -0,0 +1,30 @@
+package Easy;
+
+public class removeOuterParentheses1021 {
+
+    public static void main(String[] args) {
+
+        removeOuterParentheses("(()())(())");
+        removeOuterParentheses("()()");
+    }
+
+    static public String removeOuterParentheses(String s) {
+
+        int count = -1;
+        StringBuilder st = new StringBuilder();
+        for (Character c : s.toCharArray()) {
+            if (c == '(') {
+                count++;
+                if (count != 0)
+                    st.append(c);
+            }
+            else
+            {
+                if (count != 0)
+                    st.append(c);
+                count--;
+            }
+        }
+        return st.toString();
+    }
+}
