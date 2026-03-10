bcdb8414db4be195c44070633bc50490ec291f06
Huseyin Yatar <huseyin.yatar@ogr.sakarya.edu.tr>
2026-03-10 13:01:11 +0300

    136. Single Number
    Time Complexity
    O(log(n))
    
    Space Complexity
    O(1)

---
+++ src/main/java/Easy/ArrayPeakIndexinaMountain_852.java
@@ -0,0 +1,30 @@
+package Easy;
+
+public class ArrayPeakIndexinaMountain_852 {
+   static  public int peakIndexInMountainArray(int[] arr) {
+        int left=0;
+        int right=arr.length-1;
+        int middle=(left+right)/2;
+
+       while(right>left)
+       {
+           if(arr[middle-1]>arr[middle])
+               right=middle;
+           else if(arr[middle+1]>arr[middle])
+           {
+               left=middle;
+           }
+           else
+           {
+               return middle;
+           }
+            middle=(left+right)/2;
+
+       }
+       return middle;
+    }
+
+    public static void main(String[] args) {
+    peakIndexInMountainArray(new int[]{24,69,100,99,79,78,67,36,26,19});
+    }
+}
